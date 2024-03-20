0304372398d097a2d10bd14c9be936843cb01b36
bechir mahamat <bechirmahamat11@gmail.com>
2024-03-20 18:18:30 +0300

    login setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,21 @@
 const User = require('../Models/User')
 const bcrypt = require('bcryptjs')
-const login = (req, res) => {
-    res.send('Login login')
+const CustomAPIError = require('../errors/CustomAPIError')
+const login = async (req, res) => {
+    const { email, password } = req.body
+    if (!email || !password) {
+        throw new CustomAPIError('PLease provide email and password', 400)
+        console.log('error happened')
+    }
+    const user = await User.findOne({ email })
+    res.status(201).json({
+        user: { id: user._id, username: user.name, email: user.email },
+        token: user.createTOKEN(),
+    })
 }
 
 const register = async (req, res) => {
     const user = await User.create({ ...req.body })
-    console.log(user.createTOKEN())
     res.status(201).json({
         user: { id: user._id, username: user.name, email: user.email },
         token: user.createTOKEN(),
